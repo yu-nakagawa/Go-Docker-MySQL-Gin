62d7c895f329d1f9d745ed02dbbb919adf9d42e0
yu-nakagawa <ynakagawau@gmail.com>
2021-09-22 15:48:33 +0900

    connect mysql

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/yu-nakagawa/gin-study
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.4
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jinzhu/gorm v1.9.16
+)
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
 func main() {
+	db := sqlConnect()
+	defer db.Close()
+
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
@@ -13,4 +21,37 @@ func main() {
 	})
 
 	router.Run()
+}
+
+func sqlConnect() (database *gorm.DB) {
+	DBMS := "mysql"
+	USER := "go_test"
+	PASS := "password"
+	PROTOCOL := "tcp(db:3306)"
+	DBNAME := "go_database"
+
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+
+	count := 0
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		for {
+			if err == nil {
+				fmt.Println("")
+				break
+			}
+			fmt.Print(".")
+			time.Sleep(time.Second)
+			count++
+			if count > 180 {
+				fmt.Println("")
+				fmt.Println("DB接続失敗")
+				panic(err)
+			}
+			db, err = gorm.Open(DBMS, CONNECT)
+		}
+	}
+	fmt.Println("DB接続成功")
+
+	return db
 }
